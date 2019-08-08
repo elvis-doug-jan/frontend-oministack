7322400f14668eafd644c96bbc4c5b5396977ecc
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-08-07 22:53:13 -0300

    Adicionando rotas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.svg'
+import api from './../services/api'
 import './Login.css'
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    await api.post('/devs', {
+      username
+    }).then(res => console.log(res.data))
+
+    history.push('/main')
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev"></img>
-        <input placeholder="Digite seu usuário no Github"/>
+        <input
+          placeholder="Digite seu usuário no Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </div>
